ec6c45dcc45dcbd3194ab029abba386738050c8d
Lucky <lucky.chauhan14994.com>
2023-12-09 01:59:14 +0530

    Second commit

--- SPMWebsite/SPMWebsite/settings.py
+++ SPMWebsite/SPMWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%vsa5!+3riq7$3g)(5^i1s_qbobh9qyde%av28voc8r$q^iimj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build",'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
