462dc2332bb3bfbb27bd3aedaf9526a7f9371702
Lucky <lucky.chauhan14994.com>
2023-12-09 10:10:13 +0530

    8th Commit

--- SPMWebsite/SPMWebsite/settings.py
+++ SPMWebsite/SPMWebsite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-%vsa5!+3riq7$3g)(5^i1s_qbobh9qyde%av28voc8r$q^iimj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
-
+ALLOWED_HOSTS = ['.vercel.app','.now.sh',"127.0.0.1"]
 
 # Application definition
 
@@ -68,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'SPMWebsite.wsgi.application'
+WSGI_APPLICATION = 'SPMWebsite.wsgi.app'
 
 
 # Database
